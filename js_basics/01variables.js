5ea8991b1dfd244db0c88aaca66bb5d770a107e6
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 07:19:21 +0000

    variables commit

---
+++ js_basics/01variables.js
@@ -0,0 +1,15 @@
+const accountID = "373728";
+let username  = "waris ali";
+var useremail = "useremail123@gmail.com";
+address = "amethi";
+
+//change the value 
+
+// accountID = "37423";
+username = "waris";
+useremail = "waris253@gmail.com";
+address = "jagdishpur Amethi";
+
+console.log(accountID);
+
+console.table([accountID , username , useremail , address]);
