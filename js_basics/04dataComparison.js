7a616315a2ba9702c30fb9db949bc4573e25ea6c
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 11:16:56 +0000

    data comparison add

---
+++ js_basics/04dataComparison.js
@@ -0,0 +1,13 @@
+// console.log("2" > 2);
+// console.log(2 < "2");
+// console.log ("2" == 2);
+
+
+// console.log(null > 0 );
+// console.log(null == 0);
+// console.log(null >= 0 );
+
+
+console.log(undefined > 0 );
+console.log(undefined == 0);
+console.log(undefined >= 0 );
