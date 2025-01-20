318c30198c524d3d5b1bb2b3f36b11f1feeaaac4
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 09:57:29 +0000

    datatype conversion add

---
+++ js_basics/01datatype_conersion.js
@@ -0,0 +1,20 @@
+let score = 33 ;
+let scores = "33";
+
+console.log(typeof(score));
+console.log(typeof(scores));
+
+//change datatype conversion
+
+let scoreinNumber = Number(scores);
+console.log(typeof(scoreinNumber));
+
+let loggedIn = 0;
+let loggedInBoolean = Boolean(loggedIn);
+
+console.log(loggedInBoolean);
+
+let loggedOut = 1;
+let loggedOutBoolean = Boolean(loggedOut);
+
+console.log(loggedOutBoolean);
