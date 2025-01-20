5b93309f17aa33f084ead98dbcff7be73f7dc19d
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 12:24:52 +0000

    changes in datatype summary

--- js_basics/05datatype_summary.js
+++ js_basics/05datatype_summary.js
@@ -1,5 +1,5 @@
 
-/*primitive data type ( it copy the data)
+/*primitive data type ( it copy the value)
 7 datatype
 string
 Number
@@ -12,8 +12,33 @@ Symbol
 
 
 /*
-Reference ( Non Primitive (it gives the reference of the data))
+Reference ( Non Primitive (it gives the reference of the value))
 Array
 Objects
 Funstion
 */
+
+
+///////////////////////////////////////////////////////////////////////////////
+
+//Stack Memory (Primitive) and Heap Memory (Non Primitive)
+
+let useroneemail = "waris647@gmail.com";
+let usertwoemail = useroneemail;
+usertwoemail = "waris786@gmail.com";
+
+console.log(useroneemail);
+console.log((usertwoemail));
+//ye primitive dataype ka example hai jishme 
+
+let userone = {
+    name:"ali",
+    email:"waris9485@gmail.com"
+}
+
+let usertwo = userone;
+
+usertwo.email = "waris786@gmail.com";
+
+console.log(userone);
+
