f67c31d84050e92899cc5b79803df5585ba25e31
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 08:08:08 +0000

    datatype commit

---
+++ js_basics/01datatypes.js
@@ -0,0 +1,12 @@
+"use strict" ; //treat all js code as newer version 
+
+/*
+number  => 2 to power 53
+bignit 
+string => ""
+boolean =>"true/false"
+null => standalone value 
+undefined => define hai magar koi value assign nhi hai 
+symbol => unique
+object 
+*/
