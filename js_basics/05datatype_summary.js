ebd339c7b7ae581951cc256f90b27ef5699e2266
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-20 11:58:49 +0000

    datatype summary commit

---
+++ js_basics/05datatype_summary.js
@@ -0,0 +1,19 @@
+
+/*primitive data type ( it copy the data)
+7 datatype
+string
+Number
+boolean
+Null
+Undefined
+Bigint
+Symbol
+*/
+
+
+/*
+Reference ( Non Primitive (it gives the reference of the data))
+Array
+Objects
+Funstion
+*/
