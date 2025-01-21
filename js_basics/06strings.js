eacaa4a352ff1618bf0ba5e335f9ea8d5df7532e
Waris ali <61141431+warisa453@users.noreply.github.com>
2025-01-21 07:01:10 +0000

    string file commit

---
+++ js_basics/06strings.js
@@ -0,0 +1,28 @@
+const name = "waris ali";
+console.log(name);
+
+//old method to concat strings 
+console.log("my name is " + name);
+address = "amethi";
+//new method to concat strings
+console.log(`my name is ${name} and I'am from ${address}`);//use this method from now onwards 
+
+//another way to declare string 
+
+const gameName = new String('Waris');
+console.log(gameName);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+//slice 
+//trim
+
+
+
+
+
